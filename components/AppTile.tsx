b5bcd9094fa708b4460a576f98a7dfd1ff9d0310
kurtschambach <kurt.schambach@gmail.com>
2023-12-30 13:46:06 +0100

    feat: added iconUrls to all apps

--- components/AppTile.tsx
+++ components/AppTile.tsx
@@ -1,4 +1,5 @@
 import { AppType } from "@/data/apps";
+import Image from "next/image";
 
 type AppTileProps = {
   app: AppType;
@@ -12,9 +13,10 @@ export default function AppTile({ app, setOpen }: AppTileProps) {
       className="bg-transparent backdrop-blur-xl border-2 border-amber-300 p-6 w-40 h-40 rounded-3xl flex flex-col items-center justify-center cursor-pointer"
     >
       <div>
-        <img
+        <Image
           src={iconUrl}
-          className="mb-2 p-1 bg-amber-400/20 w-24 h-24 rounded-xl"
+          alt={`${app.name} icon`}
+          className="mb-2 p-1 bg-white/80 dark:bg-black/80 w-24 h-24 rounded-xl"
         />
       </div>
       <div>
